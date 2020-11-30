be83d31087412b2915165540e235741d6ed6b941
Khem Sok <khemsok97@gmail.com>
2020-11-29 20:52:05 -0500

    update typo

--- src/components/WhatIsRedux.js
+++ src/components/WhatIsRedux.js
@@ -10,7 +10,7 @@ export default function WhatIsRedux() {
       <Typography variant="body1" style={{ marginBottom: "70px" }}>
         <span style={{ fontSize: "1.5em", fontWeight: "700", lineHeight: "0" }}>Redux</span> is a state management library that allows users to maintain the state of the entire application in a single immutable tree. If you have worked with React
         long enough, then you know that there is some pain in dealing with handling the states when your application scales up. Moving one state from one component to the next and drilling it down further causes a lot of frustration and annoyance and
-        to be quite frank not a really good solution in handling that… Redux is able to solve all that.
+        to be quite frank it is not a really good solution in handling that… Redux is able to solve all that.
       </Typography>
 
       <div style={{ marginBottom: "60px" }}>
